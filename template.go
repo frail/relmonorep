728ee0b4e2dc8dc99260c466b787c933c49da031
Mustafa Simav <mustafa.simav@logsign.net>
2018-10-31 16:41:40 +0300

    fix: template skip sections with no commit

--- template.go
+++ template.go
@@ -7,14 +7,15 @@ const ChangeLogTemplate = `
 {{ else }}
 # {{ version }} ({{ .Date }})
 {{ end }}
-
-### Features
+{{ if .Breaking }}### Breaking Changes
+{{ range .Breaking }}
+{{ template "commit" . }}{{ end }}{{ end }}
+{{ if .Features }}### Features
 {{ range .Features }}
-{{ template "commit" . }}{{ end }}
-
-### Bug Fixes
+{{ template "commit" . }}{{ end }}{{ end }}
+{{ if .BugFixes }}### Bug Fixes
 {{ range .BugFixes }}
-{{ template "commit" . }}{{ end }}
+{{ template "commit" . }}{{ end }}{{ end }}
 {{ end }}
 
 {{ define "commit" }} * {{ .Subject }} ([{{ .ShortHash }}]({{ url }}/commit/{{ .ShortHash }})){{ end }}
